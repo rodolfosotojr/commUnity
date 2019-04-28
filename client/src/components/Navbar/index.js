8d12710b1a52bafadacbc152aaf4f89816d6c872
Purni <purnia92@gmail.com>
2019-04-28 15:55:20 -0500

    added bootstrap components and landing and signup form pages- styling still needed

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+       Home 
+      </li>
+      <li className="nav-item">
+       Community Connect
+      </li>
+      <li className="nav-item">
+       Services
+      </li>
+    </ul>
+</nav>
+  );
+}
+
+
+export default Navbar;
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,25 @@
+//this will be the sign up form page
+
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+
+function SignUp() {
+    return (
+        <div>
+
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                    <h1>Sign-Up</h1>
+                    </Col>
+                </Row>
+            </Container>
+            
+        </div>
+
+    )}
+
+export default SignUp;
