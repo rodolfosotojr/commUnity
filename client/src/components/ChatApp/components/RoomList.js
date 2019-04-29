29b29f7627aec607d111bc7106aa1d4ff28860c7
Orlando Carnate <orlando.carnate@gmail.com>
2019-04-28 22:27:28 -0500

    setup subscribe to rooms

--- client/src/components/ChatApp/components/RoomList.js
+++ client/src/components/ChatApp/components/RoomList.js
@@ -3,16 +3,16 @@ import React from "react";
 function RoomList(props) {
     return (
         <div className="rooms-list">
-                <ul>
-                    <h3>Your Rooms</h3>
-            {props.rooms.map( room => {
-                return (
+            <ul>
+                <h3>Your Rooms</h3>
+                {props.rooms.map(room => {
+                    return (
 
-                    <li key={room.id} className="room">
-                        <a href="#"># { room.name }</a>
-                    </li>
-                )
-            })}
+                        <li key={room.id} className="room">
+                            <a onClick={() => props.subscribeToRoom(room.id)} href="#"># {room.name}</a>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
