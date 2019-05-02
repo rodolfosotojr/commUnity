ed2de5d4e354d9e76be27698de4c7ff917a1f540
Orlando Carnate <orlando.carnate@gmail.com>
2019-05-02 01:23:57 -0500

    more Chatapp styling with collapsable room menu

--- client/src/components/ChatApp/components/Message.js
+++ client/src/components/ChatApp/components/Message.js
@@ -2,10 +2,32 @@ import React from "react";
 
 function Message(props) {
     return (
-        <div className="message mb-1">
-            <small className="text-muted message-username px-2">{props.username}:</small>
-            <span className="message-text bg-primary text-light rounded p-1">{props.text}</span>
-        </div>
+
+        <React.Fragment>
+            <div className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg"
+                        className="rounded-circle user_img_msg" />
+                </div>
+                <div className="msg_container px-3">
+                    {props.text}
+            <span className="msg_time">{props.username}, 8:40 AM, Today</span>
+                </div>
+            </div>
+
+            <div className="d-flex justify-content-end mb-4">
+                <div className="msg_container_send px-3">
+                    {props.text}
+                            <span className="msg_time_send">{props.username}, 8:55 AM, Today</span>
+                </div>
+                <div className="img_cont_msg">
+                    <img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg"
+                        className="rounded-circle user_img_msg" />
+                </div>
+            </div>
+
+        </React.Fragment>
+
     )
 }
 
--- client/src/components/ChatApp/components/RoomList.js
+++ client/src/components/ChatApp/components/RoomList.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function RoomList(props) {
-    const roomOrder = [...props.rooms].sort((a,b) => a.id - b.id) // sort() array method
-    return (
-        <div className="rooms-list bg-primary text-light">
-            {/* <ul> */}
-                <h5>Your Rooms</h5>
-                {roomOrder.map(room => {
-                    const active = room.id === props.roomId ? " active" : "";
+class RoomList extends Component {
+    state = {
+        open: false,
+        currentRoom: "Chat Rooms"
+    }
+
+    toggleCollapse = () => {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    // roomOrder = [...this.props.rooms].sort((a, b) => a.id - b.id) // sort() array method
+
+    render() {
+        return (
+            <div className="rooms-list bg-primary text-light">
+                <button className="btn btn-secondary dropdown-toggle" onClick={this.toggleCollapse} data-toggle="collapse">{this.state.currentRoom}</button>
+                <div id="rooms" className={'collapse' + (this.state.open ? ' show' : '')}>
+                    {[...this.props.rooms].sort((a, b) => a.id - b.id).map(room => {
+                        const active = room.id === this.props.roomId ? " active" : "";
+                        return (
+                            <div key={room.id} className={"room" + active}>
+                                <a
+                                    className="text-light"
+                                    onClick={() => {
+                                        this.props.subscribeToRoom(room.id);
+                                        this.toggleCollapse();
+                                        this.setState({ currentRoom: room.name})
+                                    }}
+                                    href="#">
+                                    <i className="fas fa-door-open"></i> {room.name}
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+                {/* {this.roomOrder.map(room => {
+                    const active = room.id === this.props.roomId ? " active" : "";
                     return (
                         <div key={room.id} className={"room" + active}>
-                            <a className="text-light" onClick={() => props.subscribeToRoom(room.id)} href="#"><i className="fas fa-door-open"></i> {room.name}</a>
+                            <a className="text-light" onClick={() => this.props.subscribeToRoom(room.id)} href="#"><i className="fas fa-door-open"></i> {room.name}</a>
                         </div>
                     )
-                })}
-            {/* </ul> */}
-        </div>
-    )
+                })} */}
+            </div>
+        )
+
+    }
 }
 
 export default RoomList;
