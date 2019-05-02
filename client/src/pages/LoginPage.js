1a43b9b271eb1c91e652ba6c9ef1a186ddc71e9a
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-05-02 14:00:24 -0500

    fixing routing

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,20 +3,17 @@ import {API_URL} from "../config"
 
 class LoginPage extends Component {
 
-    // handleclick = () => {
-    //     fetch("/auth/facebook",{
-    //         mode: "cors",
-    //         headers: {
-    //             "Content-Type": "text/json",
-    //         },
-            
-    //     }).then(console.log("i fetched"))
-    // }
+    handleclick = () => {
+        fetch(`${API_URL}/auth/facebook`).then(function(){
+            console.log("fetched!")
+        })
+    }
     render(){
         return (
             <div>
             <h1>Login</h1>
-            <a href={`${API_URL}/auth/facebook`}>facebook</a>
+            <button onClick={this.handleclick}>Facebook</button>
+            {/* <a href={`${API_URL}/auth/facebook`}>facebook</a> */}
             </div>
         )
     }
