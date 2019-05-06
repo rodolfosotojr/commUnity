d3febae2ba4426361ff00f1ee23ed86126cfacbb
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-05-06 14:10:19 -0500

    jwt login/protected route test

--- server.js
+++ server.js
@@ -5,36 +5,24 @@ const app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const passport = require("passport");
+const passport = require("./config/passport");
 const routes = require("./routes");
-
 const db = require("./models");
-
-require("./config/passport")(passport);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.use(cookieParser());
 app.use(bodyParser());
 
-// This will drop and create the db if set to true
-var syncOptions = { force: false };
-
-app.use(session({ secret: 'keyboard cat',  saveUninitialized: true,
-resave: true }));
 app.use(passport.initialize());
-app.use(passport.session());
-
-// Define API routes here
 app.use(routes)
 
-// Send every other request to the React app
-// Define any API routes before this runs
+var syncOptions = { force: false };
+
 
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, () => {
