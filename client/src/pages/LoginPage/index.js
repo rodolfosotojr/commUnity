9cb929bb2b1377e664fc24c63468f5c43887ecd4
Orlando Carnate <orlando.carnate@gmail.com>
2019-05-07 00:45:19 -0500

    update login and home pages. Stopped authentication, not working.

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
 import Col from "../../components/Col";
-import './Home.css';
+import './login.css';
 import logo from './logo1.png';
 import axios from "axios";
 
@@ -15,6 +15,16 @@ class LoginPage extends Component {
         rememberMe: false,
     }
 
+    componentDidMount() {
+        axios.post("/auth/local/protected")
+            .then(res => {
+                console.log("AUTH RESPONSE: ", res.data.username)
+                if (res.status === 200) {
+                    window.location.assign("/Home")
+                }
+
+            })
+    }
     // Using [event.target.name] to handle onChange for multiple elements using a single function
     handleInput = event => {
         this.setState({
@@ -26,64 +36,61 @@ class LoginPage extends Component {
             rememberMe: event.target.checked
         })
     }
-    handleFormSubmit = event =>
-    {
+    handleFormSubmit = event => {
         event.preventDefault();
-        axios.post("/auth/local",{
+        axios.post("/auth/local", {
             username: this.state.username,
             password: this.state.password
-        }).then((res) =>{
+        }).then((res) => {
             window.location.assign("/Home")
         })
     }
 
     render() {
         return (
-            <div className="homeComponent">
-                <div className="overlay">
-                    <Container>
-                        <Row className="row justify-content-center pt-3">
-                            <Col size="md-6" align="center">
-                                <img src={logo} />
-                                <h1>Comm<span className="font-weight-bold">Unity</span></h1>
-                            </Col>
-                        </Row>
-                        <Row className="row justify-content-center">
-                            <Col size='sm-4'>
-                                <form>
-                                    <div className="form-group">
-                                        {/* <label>Enter Username</label> */}
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            name="username"
-                                            onChange={this.handleInput}
-                                            value={this.state.username}
-                                            placeholder="Enter Username" />
-                                    </div>
-                                    <div className="form-group">
-                                        {/* <label>Password</label> */}
-                                        <input
-                                            type="password"
-                                            className="form-control"
-                                            name="password"
-                                            onChange={this.handleInput}
-                                            value={this.state.password}
-                                            placeholder="Password" />
-                                    </div>
+            <div className="login-bg">
+                <Container>
+                    <Row className="row justify-content-center py-5">
+                        <Col size="md-6" align="center">
+                            <img src={logo} />
+                            <h1 className="text-light">Comm<span className="font-weight-bold">Unity</span></h1>
+                        </Col>
+                    </Row>
+                    <Row className="row justify-content-center">
+                        <Col size='sm-5'>
+                            <form>
+                                <div className="form-group">
+                                    {/* <label>Enter Username</label> */}
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="username"
+                                        onChange={this.handleInput}
+                                        value={this.state.username}
+                                        placeholder="Enter Username" />
+                                </div>
+                                <div className="form-group">
+                                    {/* <label>Password</label> */}
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        name="password"
+                                        onChange={this.handleInput}
+                                        value={this.state.password}
+                                        placeholder="Password" />
+                                </div>
 
-                                    <div className="form-group form-check">
-                                        <input type="checkbox" className="form-check-input" id="rememberme" />
-                                        <label className="form-check-label">Remember Me</label>
-                                        <button type="submit" className="btn btn-primary float-right" onClick={this.handleFormSubmit}>Login</button>
-                                    </div>
-                                </form>
-                                <div className="text-center pt-3">Not a Member? <a href="/Register">Register Now! <i class="fas fa-user-plus"></i></a></div>
-                            </Col>
-                        </Row>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="rememberme" />
+                                    <label className="form-check-label text-light">Remember Me</label>
+                                    <button type="submit" className="btn btn-primary float-right" onClick={this.handleFormSubmit}>Login</button>
+                                </div>
+                            </form>
+                            <div className="text-center pt-3 text-light">Not a Member? <a href="/Register">Register Now! <i class="fas fa-user-plus"></i></a></div>
+                        </Col>
+                    </Row>
 
-                    </Container>
-                </div>
+                </Container>
             </div>
 
         )
