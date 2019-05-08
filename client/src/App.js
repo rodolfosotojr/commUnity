31868049ef3b1be3f5a579580503b27b01a987a8
Orlando Carnate <orlando.carnate@gmail.com>
2019-05-08 16:35:52 -0500

    adjust app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
-import Volunteer from "./components/Chat/Volunteer";
-import User from "./components/Chat/User";
-// import HomePage from "./pages/HomePage";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Connect from "./pages/Connect";
@@ -14,6 +10,7 @@ import Services from "./pages/Services";
 import LoginPage from "./pages/LoginPage"
 import Register from "./pages/Register";
 import Container from "./components/Container";
+// import Footer from "./components/Footer";
 
 
 
@@ -26,18 +23,15 @@ class App extends Component {
       
           <Switch>
             <Route exact path="/" component={LoginPage} />
-            <Route path="/Home" component={Home} />
-            <Route path="/SignUp" component={SignUp} />
-            <Route path="/Connect" component={Connect} />
-            <Route path="/Services" component={Services} />
-            <Route path="/Register" component={Register} />
-            <Route path="/Chat" component={ChatPage} />
-            <Route path="/User" component={User} />
-            <Route path="/Volunteer" component={Volunteer} />
-            {/* <Route path="/User" component={User} /> */}
+            <Route exact path="/Home" component={Home} />
+            <Route exact path="/SignUp" component={SignUp} />
+            <Route exact path="/Connect" component={Connect} />
+            <Route exact path="/Services" component={Services} />
+            <Route exact path="/Register" component={Register} />
+            <Route exact path="/Chat" component={ChatPage} />
+            <Route exact path="/User" component={User} />
           </Switch>
           {/* <Footer /> */}
-          <Navbar />
           <Navbar />
         </div>
         
