3bb86bea6e58e7a6a53e8fe19c6c7621dfee8504
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-05-08 14:42:48 -0500

    more updates to code on profile cards

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
+import "./style.css"
 
 const ProfileWrapper = styled.div`
     display: flex;
@@ -11,9 +12,10 @@ export default function ProfileCard(props) {
         <React.Fragment>
             <ProfileWrapper>
                 <div className="card">
-                    <h1>{props.firstName}</h1>
-                    <h1>{props.lastName}</h1>
+                    <h1>{props.firstName} {props.lastName}</h1>
                     <h3>{props.city}</h3>
+                    <a>{props.email}</a>
+                    <p>{props.shortBio}</p>
                     <img src={props.imageSrc}></img>
                 </div>
             </ProfileWrapper>
--- client/src/pages/Connect/index.js
+++ client/src/pages/Connect/index.js
@@ -5,61 +5,28 @@ import Container from "../../components/Container";
 import Row from "../../components/Row";
 import Col from "../../components/Col";
 import './Connect.css';
-import ProfileCard from '../../components/ProfileCard';
-import styled from "styled-components";
 import SlideView from "../../components/SlideView"
 
-
-
-
-const data = [
-    {
-        first: 'Rodolfo',
-        last: 'Soto',
-        city: "Chicago"
-    },
-    {
-        first: 'Sam',
-        last: 'Samson',
-        city: "Sacramento"
-    },
-    {
-        first: 'John',
-        last: 'Smith',
-        city: "New York City"
-    }
-
-
-]
-
 function Connect() {
-
-
     return (
         <div className="connectComponent">
-        <div className="overlay">
+            <div className="overlay">
 
-            <Container>
-                <Row>
-                    <Col size="md-12">
-                    <h1>Volunteer profiles go here!</h1>
-                    </Col>
-
-               
-                </Row>
-            </Container>
-            <Container>
-            <SlideView
-                profile="HELLO"
-            />
-
-
-            </Container>
+                <Container>
+                    <Row>
+                        <Col size="md-12">
+                            <h1>Volunteer profiles go here!</h1>
+                        </Col>
+                    </Row>
+                </Container>
                 
+                <Container>
+                    <SlideView />
+                </Container>
 
             </div>
         </div>
-
-    )}
+    )
+}
 
 export default Connect;
