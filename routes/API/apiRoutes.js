3167967a74aab554a9ff2ced96c760f878b2f295
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-05-09 17:58:40 -0500

    api routes working with database

--- routes/API/apiRoutes.js
+++ routes/API/apiRoutes.js
@@ -1,12 +1,19 @@
-var db = require("../../models");
-var passport = require("../../config/passport");
+const db = require("../../models");
+const passport = require("../../config/passport");
 
 module.exports = function (app) {
+    //GET route for getting all of my user information
+    app.get("/api/posts", function (req, res) {
+        db.User.findAll({})
+            .then(function (dbUser) {
+                res.json(dbUser);
+            });
+    });
     // LOGOUT ROUTE
     // Logout using .logout() method then redirect to login page.
     app.get("/api/logout", function (req, res) {
         req.logOut();
-        
+
         res.redirect("/");
     });
 }
