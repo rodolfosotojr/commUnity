db1c0d580d49babf2233b30d2bddd5da33bd7d32
Purni <purnia92@gmail.com>
2019-05-11 12:37:43 -0500

    added resources db

---
+++ models/Resources.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+    const Resources = sequelize.define('Resources', {
+        resource_department: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            notEmpty: true
+        },
+        org_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            notEmpty: true
+        },
+        org_description: {
+            type: DataTypes.STRING(1000),
+            allowNull: false,
+            notEmpty: true,
+             
+        },
+        org_location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            notEmpty: true
+        },
+        org_website: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            notEmpty: true
+        },
+        org_contact: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            notEmpty: true
+        },
+       
+        
+    },
+    {
+        timestamps: false
+    });
+
+    return Resources;
+}
---
+++ models/db/Seeds.sql
@@ -0,0 +1,137 @@
+
+------------------------ Education ---------------------------
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Education", "Illinois Coalition for Immigrant and Refugee Rights", "The Illinois Coalition...", "228 S. Wabash, Suite 800
+Chicago, Illinois 60604", "www.icirr.org", "312-332-7360");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Education", "Centro Romero", "Determined to bridge the gap between immigrant and refugee populations and mainstream American society, Centro Romero has been working for over 30 years to serve Chicago’s northeast side. Their many programs include youth-based learning and leadership services, adult education courses, legal support and a myriad of family programming.","6216 N. Clark St",
+ "http://centroromero.org/", "773-508-5300");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Education", "The Chatholic Charities", "For more than 40 years, Catholic Charities has helped refugees from around the world to call Chicago home. Each year, we resettle around 200 refugees who have been forced to flee their homelands due to war, terror, and persecution.","721 N. LaSalle",
+ "http://www.catholiccharities.net/GetHelp/OurServices/RefugeeResettlementServices.aspx", "312-655-7700");
+------------------------ Healthcare ---------------------------
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Healthcare", "Illinois Coalition for Immigrant and Refugee Rights", "The Illinois Coalition...", "228 S. Wabash, Suite 800
+Chicago, Illinois 60604", "www.icirr.org", "312-332-7360");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Healthcare", "Heartland Alliance", "Operating throughout the Midwest and in 20 countries internationally, Heartland Alliance provides help with housing, healthcare, employment and legal needs of immigrant, refugee and underserved populations.", "208 S. LaSalle Street, Suite 1300",
+ "https://www.heartlandalliance.org/", "312.660.1300");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Healthcare", "RefugeeOne", "Working with refugees upon arrival at the airport, RefugeeOne provides furnished housing, language courses, employment search support, mental healthcare services and community programming to aid refugees in adjusting to life in America and Chicago.","4753 N. Broadway, Suite 401",
+ "http://www.refugeeone.org/", "(773) 989-5647");
+
+------------------------ Language ---------------------------
+
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Language", "Illinois Coalition for Immigrant and Refugee Rights", "The Illinois Coalition...", "228 S. Wabash, Suite 800
+Chicago, Illinois 60604", "www.icirr.org", "312-332-7360");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Language", "World Relief Chicago", "World Relief Chicago assists refugees and immigrants navigate American culture and organization. By contributing help to find employment, apply for government aid or assistance, navigate the American immigration system, or learn the English language, World Relief Chicago is dedicated to offering a comprehensive resettlement program for immigrants and refugees from all over the world.", "3507 W Lawrence Chicago, Illinois 60625",
+ "https://worldreliefchicago.org/", "773.583.9191");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Language", "RefugeeOne", "Working with refugees upon arrival at the airport, RefugeeOne provides furnished housing, language courses, employment search support, mental healthcare services and community programming to aid refugees in adjusting to life in America and Chicago.","4753 N. Broadway, Suite 401",
+ "http://www.refugeeone.org/", "(773) 989-5647");
+
+  INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Language", "GirlFoward", "Working in both Chicago and Austin, GirlForward is an organization that focuses support on young women within refugee populations.","N/A",
+ "https://www.girlforward.org/", "(773) 856-0598");
+
+  INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Language", "The Chatholic Charities", "For more than 40 years, Catholic Charities has helped refugees from around the world to call Chicago home. Each year, we resettle around 200 refugees who have been forced to flee their homelands due to war, terror, and persecution.","721 N. LaSalle",
+ "http://www.catholiccharities.net/GetHelp/OurServices/RefugeeResettlementServices.aspx", "312-655-7700");
+
+------------------------ Legal ---------------------------
+
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Legal", "Illinois Coalition for Immigrant and Refugee Rights", "The Illinois Coalition...", "228 S. Wabash, Suite 800
+Chicago, Illinois 60604", "www.icirr.org", "312-332-7360");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Legal", "World Relief Chicago", "World Relief Chicago assists refugees and immigrants navigate American culture and organization. By contributing help to find employment, apply for government aid or assistance, navigate the American immigration system, or learn the English language, World Relief Chicago is dedicated to offering a comprehensive resettlement program for immigrants and refugees from all over the world.", "3507 W Lawrence Chicago, Illinois 60625", 
+"https://worldreliefchicago.org/", "773.583.9191");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Legal", "Heartland Alliance", "Operating throughout the Midwest and in 20 countries internationally, Heartland Alliance provides help with housing, healthcare, employment and legal needs of immigrant, refugee and underserved populations.", "208 S. LaSalle Street, Suite 1300",
+"https://www.heartlandalliance.org/", "312.660.1300");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Legal", "Centro Romero", "Determined to bridge the gap between immigrant and refugee populations and mainstream American society, Centro Romero has been working for over 30 years to serve Chicago’s northeast side. Their many programs include youth-based learning and leadership services, adult education courses, legal support and a myriad of family programming.","6216 N. Clark St",
+ "http://centroromero.org/", "773-508-5300");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Legal", "The Chatholic Charities", "For more than 40 years, Catholic Charities has helped refugees from around the world to call Chicago home. Each year, we resettle around 200 refugees who have been forced to flee their homelands due to war, terror, and persecution.","721 N. LaSalle",
+ "http://www.catholiccharities.net/GetHelp/OurServices/RefugeeResettlementServices.aspx", "312-655-7700");
+
+
+
+------------------------ Jobs ---------------------------
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Jobs", "World Relief Chicago", "World Relief Chicago assists refugees and immigrants navigate American culture and organization. By contributing help to find employment, apply for government aid or assistance, navigate the American immigration system, or learn the English language, World Relief Chicago is dedicated to offering a comprehensive resettlement program for immigrants and refugees from all over the world.", "3507 W Lawrence Chicago, Illinois 60625",
+        "https://worldreliefchicago.org/", "773.583.9191");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Jobs", "Heartland Alliance", "Operating throughout the Midwest and in 20 countries internationally, Heartland Alliance provides help with housing, healthcare, employment and legal needs of immigrant, refugee and underserved populations.", "208 S. LaSalle Street, Suite 1300",
+ "https://www.heartlandalliance.org/", "312.660.1300");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Jobs", "RefugeeOne", "Working with refugees upon arrival at the airport, RefugeeOne provides furnished housing, language courses, employment search support, mental healthcare services and community programming to aid refugees in adjusting to life in America and Chicago.","4753 N. Broadway, Suite 401",
+ "http://www.refugeeone.org/", "(773) 989-5647");
+
+  INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Job", "The Chatholic Charities", "For more than 40 years, Catholic Charities has helped refugees from around the world to call Chicago home. Each year, we resettle around 200 refugees who have been forced to flee their homelands due to war, terror, and persecution.","721 N. LaSalle",
+ "http://www.catholiccharities.net/GetHelp/OurServices/RefugeeResettlementServices.aspx", "312-655-7700");
+
+
+------------------------ Community ---------------------------
+
+INSERT INTO Resources
+    (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES
+    ("Community", "Illinois Coalition for Immigrant and Refugee Rights", "The Illinois Coalition...", "228 S. Wabash, Suite 800
+Chicago, Illinois 60604", "www.icirr.org", "312-332-7360");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "World Relief Chicago", "World Relief Chicago assists refugees and immigrants navigate American culture and organization. By contributing help to find employment, apply for government aid or assistance, navigate the American immigration system, or learn the English language, World Relief Chicago is dedicated to offering a comprehensive resettlement program for immigrants and refugees from all over the world.", "3507 W Lawrence Chicago, Illinois 60625",
+ "https://worldreliefchicago.org/", "773.583.9191");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "Heartland Alliance", "Operating throughout the Midwest and in 20 countries internationally, Heartland Alliance provides help with housing, healthcare, employment and legal needs of immigrant, refugee and underserved populations.", "208 S. LaSalle Street, Suite 1300",
+ "https://www.heartlandalliance.org/", "312.660.1300");
+
+INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "RefugeeOne", "Working with refugees upon arrival at the airport, RefugeeOne provides furnished housing, language courses, employment search support, mental healthcare services and community programming to aid refugees in adjusting to life in America and Chicago.","4753 N. Broadway, Suite 401",
+ "http://www.refugeeone.org/", "(773) 989-5647");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "GirlFoward", "Working in both Chicago and Austin, GirlForward is an organization that focuses support on young women within refugee populations.","N/A",
+ "https://www.girlforward.org/", "(773) 856-0598");
+
+ INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "Centro Romero", "Determined to bridge the gap between immigrant and refugee populations and mainstream American society, Centro Romero has been working for over 30 years to serve Chicago’s northeast side. Their many programs include youth-based learning and leadership services, adult education courses, legal support and a myriad of family programming.","6216 N. Clark St",
+ "http://centroromero.org/", "773-508-5300");
+
+  INSERT INTO Resources (resource_department, org_name, org_description, org_location, org_website, org_contact)
+VALUES ("Community", "The Chatholic Charities", "For more than 40 years, Catholic Charities has helped refugees from around the world to call Chicago home. Each year, we resettle around 200 refugees who have been forced to flee their homelands due to war, terror, and persecution.","721 N. LaSalle",
+ "http://www.catholiccharities.net/GetHelp/OurServices/RefugeeResettlementServices.aspx", "312-655-7700");
+
+
+
+
+
+
