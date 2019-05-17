2d411d1389daacebff37f3bbf3c43931d59878ab
Orlando Carnate <orlando.carnate@gmail.com>
2019-05-17 14:26:26 -0500

    navbar translations

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,35 +2,52 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { withRouter } from "react-router-dom";
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import axios from "axios";
 import "./style.css";
 
 class Navbar extends Component {
-  render() {
-   
-    return (
-      this.props.location.pathname === "/Volunteer" ||this.props.location.pathname === "/UploadImage" ||this.props.location.pathname === "/" || this.props.location.pathname === "/Register" ? null :
 
-      <nav className= "navbar navbar-expand fixed-bottom navbar-light">
-        <div>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink exact to="/Home" className="nav-link">
-                Home
-            </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/Connect" className="nav-link" activeClassName="active">
-                Connect
-            </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/Services" className="nav-link" activeClassName="active">
-                Services
-            </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+  handleLogout() {
+    axios.get("/api/logout")
+      .then((res) => {
+        window.location.assign("/")
+      })
+  }
+
+  render() {
+
+    return (
+      this.props.location.pathname === "/Volunteer" || this.props.location.pathname === "/UploadImage" || this.props.location.pathname === "/" || this.props.location.pathname === "/Register" ? null :
+
+        <nav className="navbar navbar-expand fixed-bottom navbar-light">
+          <div>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink exact to="/Home" className="nav-link">
+                  <FormattedMessage id="navbar.home" defaultMessage="Home" />
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/Connect" className="nav-link" activeClassName="active">
+                  <FormattedMessage id="navbar.connect" defaultMessage="Connect" />
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/Services" className="nav-link" activeClassName="active">
+                  <FormattedMessage id="navbar.services" defaultMessage="Services" />
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" onClick={this.handleLogout}>
+                  <FormattedMessage id="navbar.logout" defaultMessage="Logout" />
+                </a>
+              </li>
+
+
+            </ul>
+          </div>
+        </nav>
 
     )
   }
